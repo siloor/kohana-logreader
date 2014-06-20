8a9c7a8f32799827743b0124eed96d6e9dc8b212
Siloor <siloor7@gmail.com>
2014-06-20 11:32:19 +0200

    Test message location

--- classes/Kohana/Controller/LogReaderAPI.php
+++ classes/Kohana/Controller/LogReaderAPI.php
@@ -27,12 +27,31 @@ class Kohana_Controller_LogReaderAPI extends LogReader_Controller
 	// Create test message
 	public function action_create_test_message()
 	{
-		if (!LogReader::$config['tester'])
+		if (LogReader::$config['tester'])
+		{
+			$location = '';
+			
+			$ch = curl_init();
+			
+			curl_setopt($ch, CURLOPT_URL, 'http://ip-api.com/json/' . Request::$client_ip);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+			
+			$ip_infos = curl_exec($ch);
+			
+			curl_close($ch);
+			
+			if ($ip_infos && ($ip_infos = json_decode($ip_infos, TRUE)) && $ip_infos['status'] === 'success')
+			{
+				$location = $ip_infos['country'] . '/' . $ip_infos['regionName'];
+			}
+			
+			Log::instance()->add(Log::NOTICE, 'Test message created! Client '. Request::$client_ip . ' Location ' . $location . ' User-agent ' . Request::$user_agent);
+		}
+		else
 		{
 			array_push($this->data['errors'], array('code' => 600, 'text' => 'Tester is set to off!'));
 		}
-		
-		Log::instance()->add(Log::NOTICE, 'Test message created! Client '. Request::$client_ip . ' User-agent ' . Request::$user_agent);
 	}
 	
 	public function after()
